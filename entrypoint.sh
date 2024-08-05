8ba0707691172ab61eaf2cc8cf958528fde99b88
stranula <stranula@gmail.com>
2024-08-05 10:29:04 -0400

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 # Check if the /data/torrents directory exists and is not empty
-if [ ! -d "/torrents" ]; then
-  echo "Error: /torrents directory does not exist."
+if [ ! -d "/data/torrents" ]; then
+  echo "Error: /data/torrents directory does not exist."
   exit 1
-elif [ -z "$(ls -A /torrents)" ]; then
-  echo "Error: /torrents directory is empty. Deleting the directory."
-  rmdir "/torrents"
+elif [ -z "$(ls -A /data/torrents)" ]; then
+  echo "Error: /data/torrents directory is empty. Deleting the directory."
+  rmdir "/data/torrents"
   exit 1
 fi
 
