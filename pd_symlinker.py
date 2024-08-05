865567c7fd80535bf1ea7acf4271d6188d0fd8dc
stranula <stranula@gmail.com>
2024-08-05 09:36:12 -0400

    Update pd_symlinker.py

--- pd_symlinker.py
+++ pd_symlinker.py
@@ -15,9 +15,8 @@ from organisemedia import process_unaccounted_folder
 # Constants
 DEFAULT_CATALOG_PATH = '/catalog/catalog.csv'
 PROCESSED_ITEMS_FILE = '/catalog/processed_items.txt'
-SRC_DIR = os.getenv('SRC_DIR')
 DEST_DIR = os.getenv('DEST_DIR')
-src_dir: SRC_DIR
+src_dir: os.getenv('SRC_DIR')
 dest_dir = os.path.join(DEST_DIR, "shows")
 dest_dir_movies = os.path.join(DEST_DIR, "movies")
 
