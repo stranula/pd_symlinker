51e92ddc66d7982b344768895c7e07b5f57dc8c1
stranula <stranula@gmail.com>
2024-08-08 14:27:00 -0400

    Update pd_symlinker.py

--- pd_symlinker.py
+++ pd_symlinker.py
@@ -107,7 +107,7 @@ def extract_id(eid_string, preferred='imdb', fallback='tmdb'):
     return 'unknown'
 
 
-def find_best_match(torrent_dir_name, actual_title, src_dir):
+def find_best_match(torrent_file_name, actual_title, src_dir):
     try:
         dirs = os.listdir(src_dir)
         sanitized_dirs = {sanitize_title(d): d for d in dirs}
@@ -125,11 +125,11 @@ def find_best_match(torrent_dir_name, actual_title, src_dir):
             return largest_file
 
         attempts = [
-            (torrent_dir_name, dirs),
+            (torrent_file_name, dirs),
             (actual_title, dirs),
-            (sanitize_title(torrent_dir_name), dirs),
+            (sanitize_title(torrent_file_name), dirs),
             (sanitize_title(actual_title), dirs),
-            (sanitize_title(torrent_dir_name), sanitized_dirs.keys()),
+            (sanitize_title(torrent_file_name), sanitized_dirs.keys()),
             (sanitize_title(actual_title), sanitized_dirs.keys())
         ]
 
@@ -143,7 +143,7 @@ def find_best_match(torrent_dir_name, actual_title, src_dir):
             if os.path.isdir(dir_path):
                 largest_file = get_largest_file(dir_path)
                 if largest_file:
-                    best_match, score = process.extractOne(torrent_dir_name, [largest_file], scorer=fuzz.ratio)
+                    best_match, score = process.extractOne(torrent_file_name, [largest_file], scorer=fuzz.ratio)
                     if score >= 90:
                         return dir_path
 
@@ -181,190 +181,161 @@ def strip_extension(name):
 def create_symlinks_from_catalog(src_dir, dest_dir, dest_dir_movies, catalog_path):
     catalog_data = read_catalog_db()
 
-    # List all directories in the src_dir
-    src_directories = {sanitize_title(d): d for d in os.listdir(src_dir) if os.path.isdir(os.path.join(src_dir, d))}
-    
-    # Makes a list of already handled items
-    handled_items = set()
-    handled_items.update({sanitize_title(entry[13]) for entry in catalog_data if entry[16]})
-    handled_items.update({sanitize_title(entry[14]) for entry in catalog_data if entry[16]})
-    handled_items.update({sanitize_title(strip_extension(entry[13])) for entry in catalog_data if entry[16]})
-    handled_items.update({sanitize_title(strip_extension(entry[14])) for entry in catalog_data if entry[16]})
-
-    # Unprocessed directories
-    unprocessed_directories = set(src_directories.keys()) - handled_items
-
-    for directory in unprocessed_directories:
-        find_best_match(torrent_dir_name, actual_title_name, src_dir)
-
-    print(f'Unprocessed directories: {unprocessed_directories}')
-    time.sleep(60)
-    
-    processed_src_directories = set()
-
     for entry in catalog_data:
-        torrent_dir_name = entry[13]
-        actual_title_name = entry[14]
+        try:
+            eid = entry[1]
+            title = entry[2]
+            type_ = entry[3]
+            year = entry[4]
+            parent_eid = entry[5]
+            parent_title = entry[6]
+            parent_type = entry[7]
+            parent_year = entry[8]
+            grandparent_eid = entry[9]
+            grandparent_title = entry[10]
+            grandparent_type = entry[11]
+            grandparent_year = entry[12]
+            torrent_file_name = entry[13]
+            catalog_torrent_file_name = entry[13]
+            actual_title = entry[14]
+            catalog_actual_title = entry[14]
+            processed_dir_name = entry[15]
+            final_symlink_path = entry[16]
+            src_dir_path = entry[17]
 
-        if torrent_dir_name and sanitize_title(torrent_dir_name) in src_directories:
-            if entry[16]:  # Check if entry[16] is not empty or None
-                processed_src_directories.add(sanitize_title(torrent_dir_name))
-                processed_src_directories.add(sanitize_title(actual_title_name))
-                continue
-            try:
-                torrent_dir_path = find_best_match(torrent_dir_name, actual_title_name, src_dir)
+            if type_ == 'movie':
+                base_title = title
+                base_year = year
+                imdb_id = extract_id(entry[1]) if entry[1] else 'unknown'
+                if f"({base_year})" in base_title:
+                    folder_name = f"{base_title} {{imdb-{imdb_id}}}"
+                else:
+                    folder_name = f"{base_title} ({base_year}) {{imdb-{imdb_id}}}"
+                target_folder = os.path.join(dest_dir_movies, folder_name)
+
+                if not os.path.exists(target_folder):
+                    os.makedirs(target_folder, exist_ok=True)
+                    print(f"Created target folder: {target_folder}")
+
+                torrent_dir_path = find_best_match(torrent_file_name, actual_title, src_dir)
                 if not torrent_dir_path:
                     continue
-                
-                if torrent_dir_path in processed_src_directories:
-                    continue
+                print(f"Processing torrent directory: {torrent_dir_path}")
 
-                title = entry[2]
-                type_ = entry[3]
-                year = entry[4]
-                parent_title = entry[6]
-                parent_type = entry[7]
-                parent_year = entry[8]
-                grandparent_title = entry[10]
-                grandparent_type = entry[11]
-                grandparent_year = entry[12]
-                actual_title = entry[14]
-                
-                target_file_path = None  # Ensure this is defined before usage
+                largest_file = None
+                largest_size = 0
+                for file_name in os.listdir(torrent_dir_path):
+                    file_path = os.path.join(torrent_dir_path, file_name)
+                    if os.path.isfile(file_path):
+                        file_size = os.path.getsize(file_path)
+                        if file_size > largest_size:
+                            largest_size = file_size
+                            largest_file = file_name
 
-                if type_ == 'movie':
-                    base_title = title
-                    base_year = year
-                    imdb_id = extract_id(entry[1]) if entry[1] else 'unknown'
-                    if f"({base_year})" in base_title:
-                        folder_name = f"{base_title} {{imdb-{imdb_id}}}"
+                if largest_file:
+                    file_ext = os.path.splitext(largest_file)[1]
+                    resolution = extract_resolution(largest_file, parent_folder_name=torrent_dir_path, file_path=os.path.join(torrent_dir_path, largest_file))
+                    target_file_name = f"{base_title}  ({base_year}) {{imdb-{imdb_id}}} [{resolution}]{file_ext}"
+                    target_file_name = clean_filename(target_file_name)
+                    target_file_path = os.path.join(target_folder, target_file_name)
+
+                    largest_file_path = os.path.join(torrent_dir_path, largest_file)
+                    if not os.path.exists(target_file_path):
+                        try:
+                            relative_source_path = os.path.relpath(largest_file_path, os.path.dirname(target_file_path))
+                            os.symlink(relative_source_path, target_file_path)
+                            print(f"Created relative symlink: {target_file_path} -> {relative_source_path}")
+                        except OSError as e:
+                            print(f"Error creating relative symlink: {e}")
+                            target_folder = None
+
                     else:
-                        folder_name = f"{base_title} ({base_year}) {{imdb-{imdb_id}}}"
-                    target_folder = os.path.join(dest_dir_movies, folder_name)
+                        print(f"Symlink already exists: {target_file_path}")
 
-                    if not os.path.exists(target_folder):
-                        os.makedirs(target_folder, exist_ok=True)
+            else:
+                base_title = grandparent_title if grandparent_title else parent_title if parent_title else title
+                base_year = grandparent_year if grandparent_year else parent_year if parent_year else year
+                imdb_id = extract_id(entry[9]) if entry[9] else extract_id(entry[5]) if entry[5] else extract_id(entry[1]) if entry[1] else 'unknown'
+
+                if f"({base_year})" in base_title:
+                    folder_name = f"{base_title} {{imdb-{imdb_id}}}"
+                else:
+                    folder_name = f"{base_title} ({base_year}) {{imdb-{imdb_id}}}"
+                target_folder = os.path.join(dest_dir, folder_name)
+
+                if not os.path.exists(target_folder):
+                    try:
+                        os.makedirs(target_folder)
                         print(f"Created target folder: {target_folder}")
-
-                    torrent_dir_path = find_best_match(torrent_dir_name, actual_title, src_dir)
-                    if not torrent_dir_path:
+                    except OSError as e:
+                        print(f"Error creating target folder: {e}")
                         continue
-                    print(f"Processing torrent directory: {torrent_dir_path}")
 
-                    largest_file = None
-                    largest_size = 0
-                    for file_name in os.listdir(torrent_dir_path):
-                        file_path = os.path.join(torrent_dir_path, file_name)
-                        if os.path.isfile(file_path):
-                            file_size = os.path.getsize(file_path)
-                            if file_size > largest_size:
-                                largest_size = file_size
-                                largest_file = file_name
+                torrent_dir_path = find_best_match(torrent_file_name, actual_title, src_dir)
+                if not torrent_dir_path:
+                    continue
+                print(f"Processing torrent directory: {torrent_dir_path}")
 
-                    if largest_file:
-                        file_ext = os.path.splitext(largest_file)[1]
-                        resolution = extract_resolution(largest_file, parent_folder_name=torrent_dir_path, file_path=os.path.join(torrent_dir_path, largest_file))
-                        target_file_name = f"{base_title}  ({base_year}) {{imdb-{imdb_id}}} [{resolution}]{file_ext}"
+                for file_name in os.listdir(torrent_dir_path):
+                    file_path = os.path.join(torrent_dir_path, file_name)
+                    print(f"Processing file: {file_path}")
+
+                    if os.path.isfile(file_path):
+                        file_ext = os.path.splitext(file_name)[1]
+
+                        season, episode = extract_season_episode(file_name)
+                        if not (season and episode):
+                            continue
+
+                        season_folder = f"Season {season}"
+                        episode_identifier = f"S{season}E{episode}"
+
+                        target_folder_season = os.path.join(target_folder, season_folder)
+                        existing_files = os.listdir(target_folder_season) if os.path.exists(target_folder_season) else []
+                        episode_pattern = f"{base_title} ({base_year}) {{imdb-{imdb_id}}} - {episode_identifier} ["
+                        if any(f.startswith(episode_pattern) and f.endswith(file_ext) for f in existing_files):
+                            print(f"Symlink for {episode_identifier} already exists. Skipping file: {file_name}...Is this causing me problems???")
+                            continue
+
+                        resolution = extract_resolution(file_name, parent_folder_name=torrent_dir_path, file_path=file_path)
+                        target_file_name = f"{base_title} ({base_year}) {{imdb-{imdb_id}}} - {episode_identifier} [{resolution}]{file_ext}"
                         target_file_name = clean_filename(target_file_name)
-                        target_file_path = os.path.join(target_folder, target_file_name)
-                        
-                        largest_file_path = os.path.join(torrent_dir_path, largest_file)
-                        if not os.path.exists(target_file_path):
+
+                        if not os.path.exists(target_folder_season):
+                            os.makedirs(target_folder_season, exist_ok=True)
+
+                        target_file_path = os.path.join(target_folder_season, target_file_name)
+
+                        if not os.path.exists(file_path):
+                            print(f"Source file does not exist: {file_path}")
+                        elif not os.path.exists(target_file_path):
                             try:
-                                relative_source_path = os.path.relpath(largest_file_path, os.path.dirname(target_file_path))
+                                relative_source_path = os.path.relpath(file_path, os.path.dirname(target_file_path))
                                 os.symlink(relative_source_path, target_file_path)
                                 print(f"Created relative symlink: {target_file_path} -> {relative_source_path}")
+
                             except OSError as e:
                                 print(f"Error creating relative symlink: {e}")
                                 target_folder = None
-
-                        else:
-                            print(f"Symlink already exists: {target_file_path}")
-
-                else:
-                    base_title = grandparent_title if grandparent_title else parent_title if parent_title else title
-                    base_year = grandparent_year if grandparent_year else parent_year if parent_year else year
-                    imdb_id = extract_id(entry[9]) if entry[9] else extract_id(entry[5]) if entry[5] else extract_id(entry[1]) if entry[1] else 'unknown'
-
-                    if f"({base_year})" in base_title:
-                        folder_name = f"{base_title} {{imdb-{imdb_id}}}"
-                    else:
-                        folder_name = f"{base_title} ({base_year}) {{imdb-{imdb_id}}}"
-                    target_folder = os.path.join(dest_dir, folder_name)
-
-                    if not os.path.exists(target_folder):
-                        try:
-                            os.makedirs(target_folder)
-                            print(f"Created target folder: {target_folder}")
-                        except OSError as e:
-                            print(f"Error creating target folder: {e}")
-                            continue
-                            
-                    torrent_dir_path = find_best_match(torrent_dir_name, actual_title, src_dir)
-                    if not torrent_dir_path:
-                        continue
-                    print(f"Processing torrent directory: {torrent_dir_path}")
-
-                    for file_name in os.listdir(torrent_dir_path):
-                        file_path = os.path.join(torrent_dir_path, file_name)
-                        print(f"Processing file: {file_path}")
-
-                        if os.path.isfile(file_path):
-                            file_ext = os.path.splitext(file_name)[1]
-
-                            season, episode = extract_season_episode(file_name)
-                            if not (season and episode):
-                                continue
-
-                            season_folder = f"Season {season}"
-                            episode_identifier = f"S{season}E{episode}"
-
-                            target_folder_season = os.path.join(target_folder, season_folder)
-                            existing_files = os.listdir(target_folder_season) if os.path.exists(target_folder_season) else []
-                            episode_pattern = f"{base_title} ({base_year}) {{imdb-{imdb_id}}} - {episode_identifier} ["
-                            if any(f.startswith(episode_pattern) and f.endswith(file_ext) for f in existing_files):
-                                print(f"Symlink for {episode_identifier} already exists. Skipping file: {file_name}...Is this causing me problems???")
-                                continue
-
-                            resolution = extract_resolution(file_name, parent_folder_name=torrent_dir_path, file_path=file_path)
-                            target_file_name = f"{base_title} ({base_year}) {{imdb-{imdb_id}}} - {episode_identifier} [{resolution}]{file_ext}"
-                            target_file_name = clean_filename(target_file_name)
-
-                            if not os.path.exists(target_folder_season):
-                                os.makedirs(target_folder_season, exist_ok=True)
-
-                            target_file_path = os.path.join(target_folder_season, target_file_name)
-
-                            if not os.path.exists(file_path):
-                                print(f"Source file does not exist: {file_path}")
-                            elif not os.path.exists(target_file_path):
-                                try:
-                                    relative_source_path = os.path.relpath(file_path, os.path.dirname(target_file_path))
-                                    os.symlink(relative_source_path, target_file_path)
-                                    print(f"Created relative symlink: {target_file_path} -> {relative_source_path}")
-
-                                except OSError as e:
-                                    print(f"Error creating relative symlink: {e}")
-                                    target_folder = None
-                if target_folder:
-                    update_catalog_entry(
-                        processed_dir_name=torrent_dir_name,
-                        final_symlink_path=target_folder,
-                        original_torrent_file_name=torrent_dir_name,
-                        original_actual_name=actual_title_name,
-                        src_dir_path=src_dir  # Include the src_dir path
-                    )
-                    processed_src_directories.add(sanitize_title(torrent_dir_name))
+            if target_folder:
+                update_catalog_entry(
+                    processed_dir_name=torrent_file_name,
+                    final_symlink_path=target_folder,
+                    original_torrent_file_name=catalog_torrent_file_name,
+                    original_actual_name=catalog_actual_title_name,
+                    src_dir_path=src_dir  # Include the src_dir path
+                )
+                processed_src_directories.add(sanitize_title(torrent_file_name))
                 
-            except Exception as e:
-                print(f"Error processing entry: {e}")
+        except Exception as e:
+            print(f"Error processing entry: {e}")
 
-    # Process directories in src_dir that are not part of the catalog
-    unprocessed_directories = set(src_directories.keys()) - processed_src_directories
-    for dir_name in unprocessed_directories:
-        dir_path = os.path.join(src_dir, src_directories[dir_name])
-        print(f"Processing unaccounted folder: {dir_path}")
-        process_unaccounted_folder(dir_path, DEST_DIR)  # Replace with your actual processing function
+    # # Process directories in src_dir that are not part of the catalog
+    # unprocessed_directories = set(src_directories.keys()) - processed_src_directories
+    # for dir_name in unprocessed_directories:
+    #     dir_path = os.path.join(src_dir, src_directories[dir_name])
+    #     print(f"Processing unaccounted folder: {dir_path}")
+    #     process_unaccounted_folder(dir_path, DEST_DIR)  # Replace with your actual processing function
 
 
 def create_symlinks():
