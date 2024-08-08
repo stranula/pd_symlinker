c5767053000826ee65e8253ce7d1a657820cb4ce
stranula <stranula@gmail.com>
2024-08-07 22:11:05 -0400

    Update pd_symlinker.py

--- pd_symlinker.py
+++ pd_symlinker.py
@@ -165,7 +165,7 @@ def extract_season_episode(file_name):
 
 def create_symlinks_from_catalog(src_dir, dest_dir, dest_dir_movies, catalog_path):
     catalog_data = read_catalog_db()
-    processed_items = {entry[15] for entry in catalog_data if entry[5]}
+    processed_items = {entry[15] for entry in catalog_data if entry[15]}
     print(processed_items)
     time.sleep(30)
 
