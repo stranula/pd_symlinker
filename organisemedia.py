3951fe4c3af031012874a56273c0b66da11f2a29
stranula <stranula@gmail.com>
2024-08-08 10:45:46 -0400

    Update organisemedia.py

--- organisemedia.py
+++ organisemedia.py
@@ -8,5 +8,5 @@ import asyncio
 import aiohttp
 from colorama import init, Fore, Style
 
-def process_unaccounted_folder
-    print("Here we are!"
+def process_unaccounted_folder:
+    print("Here we are!")
